2add7ad04cabe6ab6c057fa7282431316fc53e2e
nsmirnova <smirnovanastya1602@gmail.com>
2016-07-29 13:57:29 +0300

    Change palette model

--- app/models/palette.js
+++ app/models/palette.js
@@ -1,6 +1,6 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+import DS from 'ember-data';
 
-export default Model.extend({
-  id: attr('string')
+export default DS.Model.extend({
+  id: DS.attr('string'),
+  colours: DS.attr()
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,46 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return [{ id: '123' }, { id: '234' }, { id: '456' }];
+    return [
+      {
+        id: '123',
+        colours: [
+          {
+            id: 1,
+            colour: '#aaa'
+          },
+          {
+            id: 2,
+            colour: '#abc'
+          }
+        ]
+       },
+      {
+        id: '234',
+        colours: [
+          {
+            id: 1,
+            colour: '#fba'
+          },
+          {
+            id: 2,
+            colour: '#abc'
+          }
+        ]
+       },
+      {
+        id: '456',
+        colours: [
+          {
+            id: 1,
+            colour: '#acb'
+          },
+          {
+            id: 2,
+            colour: '#abc'
+          }
+        ]
+       }
+    ];
   }
 });
